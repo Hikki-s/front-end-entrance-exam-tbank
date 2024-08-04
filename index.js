6b9cb0191030c9a8cb382f6800f1a9f81bf29605
Hikki-s <bogdanpleshkov2002@gmail.com>
2024-08-04 20:02:57 +0300

    Updates

---
+++ index.js
@@ -0,0 +1,35 @@
+import MySqlLogo from '/mysql-svgrepo-com.svg';
+import RedisLogo from '/redis-svgrepo-com.svg';
+import MongoDBLogo from '/mongodb-svgrepo-com.svg';
+
+import JavaLogo from '/java-4-logo-svgrepo-com.svg';
+import TsLogo from '/logo-ts-svgrepo-com.svg';
+import JsLogo from '/javascript-svgrepo-com.svg';
+import Logo1C from '/icons8-1c.svg';
+import PythonLogo from '/python-svgrepo-com.svg';
+
+import ReactLogo from '/react-svgrepo-com.svg';
+import NodeLogo from '/nodejs-svgrepo-com.svg';
+import MobXLogo from '/mobx-svgrepo-com.svg';
+import GraphQLLogo from '/apollographql-svgrepo-com.svg';
+import JestLogo from '/jest-snapshot-svgrepo-com.svg';
+import DiscordJsLogo from '/discord-v2-svgrepo-com.svg';
+
+const logos = {
+    MySqlLogo,
+    RedisLogo,
+    MongoDBLogo,
+    JavaLogo,
+    TsLogo,
+    JsLogo,
+    Logo1C,
+    PythonLogo,
+    ReactLogo,
+    NodeLogo,
+    MobXLogo,
+    GraphQLLogo,
+    JestLogo,
+    DiscordJsLogo,
+};
+
+export default logos;
